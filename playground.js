9a427cd5723371bf8149ad08e8d94f421f2033a5
excellence <excellenceariweriokuma@gmail.com>
2018-02-26 18:47:44 +0000

    complete stage 1

--- playground.js
+++
@@ -1,7 +0,0 @@
-let {utilis} = require('./model/helper');
-const randomstring = require('randomstring');
-
-const sha256 = require('js-sha256');
-
-console.log(sha256.hmac('key','password'));
-console.log(sha256.hmac('key','password'));
